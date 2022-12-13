3de636379b58c9a4ce0b1657f1c855c6b2e326a5
Wars <wars@wars.cat>
2022-12-13 23:20:00 +0800

    Fix

--- darwin/home.nix
+++ darwin/home.nix
@@ -40,33 +40,33 @@
 #        pfetch
 #      '';
     };
-#    neovim = {
-#      enable = true;
-#      viAlias = true;
-#      vimAlias = true;
-#
-#      plugins = with pkgs.vimPlugins; [
-#
-#        # Syntax
-#        vim-nix
-#        vim-markdown
-#
-#        # Quality of life
-#        vim-lastplace                             # Opens document where you left it
-#        auto-pairs                                # Print double quotes/brackets/etc.
-#        vim-gitgutter                             # See uncommitted changes of file :GitGutterEnable
-#
-#        # File Tree
-#        nerdtree                                  # File Manager - set in extraConfig to F6
-#
-#        # Customization
-#        wombat256-vim                             # Color scheme for lightline
-#        srcery-vim                                # Color scheme for text
-#
-#        lightline-vim                             # Info bar at bottom
-#        indent-blankline-nvim                     # Indentation lines
-#      ];
-#
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+
+      plugins = with pkgs.vimPlugins; [
+
+        # Syntax
+        vim-nix
+        vim-markdown
+
+        # Quality of life
+        vim-lastplace                             # Opens document where you left it
+        auto-pairs                                # Print double quotes/brackets/etc.
+        vim-gitgutter                             # See uncommitted changes of file :GitGutterEnable
+
+        # File Tree
+        nerdtree                                  # File Manager - set in extraConfig to F6
+
+        # Customization
+        wombat256-vim                             # Color scheme for lightline
+        srcery-vim                                # Color scheme for text
+
+        lightline-vim                             # Info bar at bottom
+        indent-blankline-nvim                     # Indentation lines
+      ];
+
 #      extraConfig = ''
 #        syntax enable                             " Syntax highlighting
 #        colorscheme srcery                        " Color scheme text
@@ -82,6 +82,6 @@
 #
 #        nmap <F6> :NERDTreeToggle<CR>             " F6 opens NERDTree
 #      '';
-#    };
+    };
   };
 }
