e0216c6427fd207133eb9033839d1d397cee06c9
Wars <wars@wars.cat>
2022-12-13 23:12:18 +0800

    Fix ^M

--- darwin/home.nix
+++ darwin/home.nix
@@ -34,11 +34,11 @@
         custom = "$HOME/.config/zsh_nix/custom";
       };
 
-      initExtra = ''
-        source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
-        autoload -U promptinit; promptinit
-        pfetch
-      '';
+#      initExtra = ''
+#        source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
+#        autoload -U promptinit; promptinit
+#        pfetch
+#      '';
     };
     neovim = {
       enable = true;
