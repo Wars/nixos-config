f442ced277fa5efb5fe127a4120d970c5ed34c61
Wars <wars@wars.cat>
2022-12-13 22:45:10 +0800

    feat(all): initial.

--- darwin/default.nix
+++ darwin/default.nix
@@ -14,7 +14,7 @@ let
   system = "aarch64-darwin";                                 # System architecture
 in
 {
-  macbook = darwin.lib.darwinSystem {                       # MacBook8,1 "Core M" 1.2 12" (2015) A1534 ECM2746 profile
+  mcat = darwin.lib.darwinSystem {                       # MacBook8,1 "Core M" 1.2 12" (2015) A1534 ECM2746 profile
     inherit system;
     specialArgs = { inherit user inputs; };
     modules = [                                             # Modules that are used
